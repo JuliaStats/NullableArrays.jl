bf1ac7b8ad7c8ad511f90947b6fd1cc2eede3bcd
David Gold <david.gold.a@gmail.com>
2015-06-08 14:43:48 -0400

    Add outer constructors for NullableArray

--- src/02_constructors.jl
+++ src/02_constructors.jl
@@ -1,3 +1,13 @@
+
+# ----- Constructors -------------------------------------------------------- #
+
+# The following allows for construction of a NullableArray without explicit
+# specification of type parameters.
+# See docs.julialang.org/en/latest/manual/constructors/#parametric-constructors
+function NullableArray(m::AbstractArray, a::AbstractArray) # -> NullableArray
+    return NullableArray{eltype(a), ndims(a)}(m, a)
+end
+
 # TODO: Uncomment this doc entry when Base Julia can parse it correctly.
 # @doc """
 # Allow users to construct a quasi-uninitialized `NullableArray` object by
@@ -10,9 +20,23 @@
 # will be initialized to `true` everywhere, making every entry of a new
 # `NullableArray` a null value by default.
 # """ ->
-function NullableArray{T}(::Type{T}, dims::Dims)
-    NullableArray(
-        fill(true, dims),
-        Array(T, dims),
-    )
+function NullableArray{T}(::Type{T}, dims::Dims) # -> NullableArray
+    return NullableArray(fill(true, dims), Array(T, dims))
+end
+
+# Constructs an empty NullableArray of type parameter T and number of dimensions
+# equal to the number of arguments given in 'dims...', where the latter are
+# dimension lengths.
+function NullableArray(T::Type, dims::Int...) # -> NullableArray
+    return NullableArray(T, dims)
+end
+
+# Constructs a NullableArray from an Array 'a' of values and an optional
+# Array{Bool, N} mask. If omitted, the mask will default to an array of
+# 'false's the size of 'a'.
+function NullableArray{T, N}(
+    a::AbstractArray{T, N},
+    m::Array{Bool,N} = fill(false, size(a))
+) # -> NullableArray
+    return NullableArray(m, a)
 end
