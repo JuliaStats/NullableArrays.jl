166cf1e847553df11931a71ab7f8e71b10720389
David Gold <david.gold.a@gmail.com>
2015-06-09 12:23:37 -0400

    Remove tests for obsolete constructor method
    One cannot construct a NullableArray by passing first the 'values' arg and then the 'insole' arg; one must always pass the 'isnull' boolean mask first.

--- test/02_constructors.jl
+++ test/02_constructors.jl
@@ -6,15 +6,12 @@ module TestConstructors
     x1 = NullableArray(Int, 5)
     x2 = NullableArray(Int, 5, 2)
     y = NullableArray([1, 2, 3, 4, 5, 6])
-    y1 = NullableArray([1, 2, 3, 4, 5, 6], [true, false, false, false, false ,false])
     y2 = NullableArray([true, false, false, false, false ,false], [1, 2, 3, 4, 5, 6])
 
     @test isa(x, NullableVector{Int})
     @test isa(x1, NullableVector{Int})
     @test isa(x2, NullableMatrix{Int})
     @test isa(y, NullableVector{Int})
-    @test isa(y1, NullableVector{Int})
-    @test y1.isnull[1]
     @test isa(y2, NullableVector{Int})
     @test y2.isnull[1]
 end
