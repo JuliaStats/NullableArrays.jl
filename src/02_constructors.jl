5e0fe26122cce957895c462e7315705dfa5eb467
David Gold <david.gold.a@gmail.com>
2015-06-12 13:28:15 -0400

    Revise outer constructors
    -change arg signature on first outer constructor 
    to match that of inner constructor
    -add new outer constructor to handle pseudo-
    literal arrays w/ missing values

--- src/02_constructors.jl
+++ src/02_constructors.jl
@@ -4,8 +4,8 @@
 # The following allows for construction of a NullableArray without explicit
 # specification of type parameters;
 # see docs.julialang.org/en/latest/manual/constructors/#parametric-constructors
-function NullableArray(m::AbstractArray, a::AbstractArray) # -> NullableArray
-    return NullableArray{eltype(a), ndims(a)}(m, a)
+function NullableArray{T, N}(m::Array{Bool, N}, a::AbstractArray{T, N}) # -> NullableArray
+    return NullableArray{T, N}(m, a)
 end
 
 # TODO: Uncomment this doc entry when Base Julia can parse it correctly.
@@ -36,4 +36,36 @@ end
 # 'false's the size of 'a'.
 function NullableArray{T, N}(a::AbstractArray{T, N}) # -> NullableArray
     return NullableArray{T, N}(fill(false, size(a)), a)
+end
+
+# The following method constructs a NullableArray from an Array{Any} argument
+# 'A' that contains some placeholder of type 'T' for null values.
+#
+# e.g.: julia> NullableArray([1, nothing, 2], Void)
+#       3-element NullableArrays.NullableArray{Int64,1}:
+#       Nullable(1)
+#       Nullable{Int64}()
+#       Nullable(2)
+#
+#       julia> NullableArray([1, "notdefined", 2], ASCIIString)
+#       3-element NullableArrays.NullableArray{Int64,1}:
+#       Nullable(1)
+#       Nullable{Int64}()
+#       Nullable(2)
+#
+# TODO: think about dispatching on T = Any in method above to call
+# the following method passing 'T=Void' for pseudo-literal
+# NullableArray construction
+function NullableArray{T, U}(A::Array, ::Type{T}, ::Type{U})
+    _isnull = fill(true, size(A))
+    _values = Array(T, size(A))
+    @inbounds for (i, value) in enumerate(A)
+        if !isa(value, U)
+            setindex!(_isnull, false, i)
+        end
+    end
+    @inbounds for (i, isnull) in enumerate(_isnull)
+        !isnull && setindex!(_values, A[i], i)
+    end
+    return NullableArray(_isnull, _values)
 end
