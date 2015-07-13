74673dfeb87390bd935475ae98bd5684def2e6c6
David Gold <david.gold.a@gmail.com>
2015-07-13 10:55:01 -0400

    Revise broadcast profiling methods

--- perf/broadcast.jl
+++ perf/broadcast.jl
@@ -38,8 +38,7 @@ I2 = DataArray(C2, M2)
 
 f(x, y) = x * y
 
-function profile_broadcast()
-
+function profile_broadcast(f, L, U, D, A1, A2, X1, X2, E1, E2, Y1, Y2, F1, F2)
     println("f(x, y) := x * y")
     println("Method: broadcast!(f, dest, A1, A2) (no empty entries):")
     broadcast!(f, L, A1, A2)
@@ -66,7 +65,7 @@ function profile_broadcast()
     nothing
 end
 
-function profile_ops1()
+function profile_ops_nonulls(A1, A2, X1, X2, E1, E2)
     for op in (
         :(.+),
         :(.-),
@@ -89,17 +88,6 @@ function profile_ops1()
             @time($_op(E1, E2))
         end
     end
-
-    println("Method: .>> (no empty entries)")
-    .>>(C2, C1)
-    .>>(V2, V1)
-    .>>(H2, H1)
-    print("  For Array{Float64}:          ")
-    @time(.>>(C2, C1))
-    print("  For NullableArray{Float64}:  ")
-    @time(.>>(Q2, Q1))
-    print("  For DataArray{Float64}:      ")
-    @time(.>>(H2, H1))
 
     for op in (
         :(.==),
@@ -126,7 +114,31 @@ function profile_ops1()
     nothing
 end
 
-function profile_ops2()
+function profile_broadcasted_right(C1, C2, V1, V2, H1, H2, Q1, Q2)
+    println("Method: .>> (no empty entries)")
+    .>>(C2, C1)
+    .>>(V2, V1)
+    .>>(H2, H1)
+    print("  For Array{Float64}:          ")
+    @time(.>>(C2, C1))
+    print("  For NullableArray{Float64}:  ")
+    @time(.>>(V2, V1))
+    print("  For DataArray{Float64}:      ")
+    @time(.>>(H2, H1))
+
+    println("Method: .>> (~half empty entries)")
+    .>>(C2, C1)
+    .>>(Q2, Q1)
+    # .>>(I2, I1)
+    print("  For Array{Float64}:          ")
+    @time(.>>(C2, C1))
+    print("  For NullableArray{Float64}:  ")
+    @time(.>>(Q2, Q1))
+    # print("For DataArray{Float64}:      ")
+    # @time(.>>(I2, I1))
+end
+
+function profile_ops_halfnulls(A1, A2, Y1, Y2, F1, F2)
     for op in (
         :(.+),
         :(.-),
@@ -149,17 +161,6 @@ function profile_ops2()
             @time($_op(F1, F2))
         end
     end
-
-    println("Method: .>> (~half empty entries)")
-    .>>(C2, C1)
-    .>>(V2, V1)
-    # .>>(I2, I1)
-    print("  For Array{Float64}:          ")
-    @time(.>>(C2, C1))
-    print("  For NullableArray{Float64}:  ")
-    @time(.>>(V2, V1))
-    # print("For DataArray{Float64}:      ")
-    # @time(.>>(I2, I1))
 
     for op in (
         :(.==),
@@ -184,4 +185,11 @@ function profile_ops2()
         end
     end
     nothing
+end
+
+function profile_broadcast_all()
+    profile_broadcast(f, L, U, D, A1, A2, X1, X2, E1, E2, Y1, Y2, F1, F2)
+    profile_ops_nonulls(A1, A2, X1, X2, E1, E2)
+    profile_ops_halfnulls(A1, A2, Y1, Y2, F1, F2)
+    profile_broadcasted_right(C1, C2, V1, V2, H1, H2, Q1, Q2)
 end
