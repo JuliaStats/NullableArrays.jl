0a006224ee3cd1040f0af0d727fcf6588f50e9f6
Milan Bouchet-Valat <nalimilan@club.fr>
2016-12-06 17:17:24 +0100

    Use generated function to avoid allocations on Julia 0.5
    The 1- and 2-argument versions are slower on 0.5 due to allocations,
    but faster (especially the latter) on 0.6.

--- src/lift.jl
+++ src/lift.jl
@@ -1,44 +1,41 @@
-@inline function lift(f, x)
-    if null_safe_op(f, eltype(x))
-        return @compat Nullable(f(unsafe_get(x)), !isnull(x))
-    else
-        U = Core.Inference.return_type(f, Tuple{eltype(x)})
-        if isnull(x)
-            return isleaftype(U) ? Nullable{U}() : Nullable()
-        else
-            return Nullable(f(unsafe_get(x)))
-        end
-    end
-end
-
-@inline function lift(f, x1, x2)
-    if null_safe_op(f, eltype(x1), eltype(x2))
-        return @compat Nullable(f(unsafe_get(x1), unsafe_get(x2)),
-                                !(isnull(x1) | isnull(x2)))
-    else
-        U = Core.Inference.return_type(f, Tuple{eltype(x1), eltype(x2)})
-        if isnull(x1) | isnull(x2)
-            return isleaftype(U) ? Nullable{U}() : Nullable()
-        else
-            return Nullable(f(unsafe_get(x1), unsafe_get(x2)))
-        end
-    end
-end
 
 eltype_nullable(x::Nullable) = eltype(x)
 eltype_nullable(x) = typeof(x)
+eltype_nullable{T<:Nullable}(::Type{T}) = eltype(T)
+eltype_nullable{T}(::Type{T}) = T
+
+if VERSION >= v"0.6.0-dev"
+    @inline function lift{F}(f::F, x)
+        if null_safe_op(f, eltype_nullable(x))
+            return @compat Nullable(f(unsafe_get(x)), !isnull(x))
+        else
+            U = Core.Inference.return_type(f, Tuple{eltype(x)})
+            if isnull(x)
+                return isleaftype(U) ? Nullable{U}() : Nullable()
+            else
+                return Nullable(f(unsafe_get(x)))
+            end
+        end
+    end
+
+    @inline function lift{F}(f::F, x1, x2)
+        if null_safe_op(f, eltype_nullable(x1), eltype_nullable(x2))
+            return @compat Nullable(f(unsafe_get(x1), unsafe_get(x2)),
+                                    !(isnull(x1) | isnull(x2)))
+        else
+            U = Core.Inference.return_type(f, Tuple{eltype(x1), eltype(x2)})
+            if isnull(x1) | isnull(x2)
+                return isleaftype(U) ? Nullable{U}() : Nullable()
+            else
+                return Nullable(f(unsafe_get(x1), unsafe_get(x2)))
+            end
+        end
+    end
+end
 
 eltypes() = Tuple{}
 eltypes(x) = Tuple{eltype_nullable(x)}
 eltypes(x, xs...) = Tuple{eltype_nullable(x), eltypes(xs...).parameters...}
-
-hasnulls() = false
-hasnulls(x) = isnull(x)
-hasnulls(x, xs...) = hasnulls(x) | hasnulls(xs...)
-
-_unsafe_get() = ()
-_unsafe_get(x) = (unsafe_get(x),)
-_unsafe_get(x, xs...) = (unsafe_get(x), _unsafe_get(xs...)...)
 
 """
   lift(f, xs...)
@@ -47,15 +44,29 @@ Lift function `f`, passing it arguments `xs...`, using standard lifting semantic
 for a function call `f(xs...)`, return null if any `x` in `xs` is null; otherwise,
 return `f` applied to values of `xs`.
 """
-@inline function lift(f, xs...)
-    if null_safe_op(f, map(eltype_nullable, xs)...)
-        return @compat Nullable(f(_unsafe_get(xs...)...), !hasnulls(xs...))
-    else
-        U = Core.Inference.return_type(f, eltypes(xs...))
-        if hasnulls(xs...)
-            return isleaftype(U) ? Nullable{U}() : Nullable()
-        else
-            return Nullable(f(_unsafe_get(xs...)...))
+@generated function lift{F, T, N}(f::F, xs::Vararg{T, N})
+    args = (:(unsafe_get(xs[$i])) for i in 1:N)
+    checknull = (:(isnull(xs[$i])) for i in 1:N)
+    if null_safe_op(f.instance, eltypes(xs...).parameters...)
+        return quote
+            @Base._inline_meta
+            @inbounds val = f($(args...))
+            @inbounds hasnulls = |($(checknull...))
+            @compat Nullable(val, !hasnulls)
         end
+    else
+      return quote
+          @Base._inline_meta
+          U = Core.Inference.return_type(f, eltypes(xs...))
+          @inbounds hasnulls = |($(checknull...))
+          if hasnulls
+              return isleaftype(U) ? Nullable{U}() : Nullable()
+          else
+              @inbounds val = f($(args...))
+              return Nullable(val)
+          end
+      end
     end
 end
+
+lift(f) = Nullable(f())
