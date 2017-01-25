e9747bf6f33b6e060720e6740b03688df7d7a348
Milan Bouchet-Valat <nalimilan@club.fr>
2017-01-25 22:02:27 +0100

    Fix broadcast() and map() on Julia 0.5
    Non-singleton types do not have an 'instance' field, so we cannot call
    null_safe_op() from a generated function for their constructors.
    This means they will always be considered as unsafe. Also add a branch for
    Julia 0.5, where inference does not work: the return type needs to be
    computed from the generated function.
    
    Finally, fix computing the element type of the array returned by broadcast()
    on Julia 0.5.

--- src/lift.jl
+++ src/lift.jl
@@ -7,7 +7,7 @@ eltypes(x) = Tuple{eltype_nullable(x)}
 eltypes(x, xs...) = Tuple{eltype_nullable(x), eltypes(xs...).parameters...}
 
 """
-  lift(f, xs...)
+    lift(f, xs...)
 
 Lift function `f`, passing it arguments `xs...`, using standard lifting semantics:
 for a function call `f(xs...)`, return null if any `x` in `xs` is null; otherwise,
@@ -17,19 +17,35 @@ return `f` applied to values of `xs`.
     N = nfields(xs)
     args = (:(unsafe_get(xs[$i])) for i in 1:N)
     checknull = (:(!isnull(xs[$i])) for i in 1:N)
-    if null_safe_op(f.instance, map(eltype_nullable, xs.parameters)...)
+    if isdefined(f, :instance) && null_safe_op(f.instance, map(eltype_nullable, xs.parameters)...)
         return quote
             val = f($(args...))
             nonull = (&)($(checknull...))
             @compat Nullable(val, nonull)
         end
-    else
+    elseif VERSION >= v"0.6.0-dev"
       return quote
           U = Core.Inference.return_type(f, eltypes(xs...))
           if (&)($(checknull...))
               return Nullable(f($(args...)))
           else
               return isleaftype(U) ? Nullable{U}() : Nullable()
+          end
+      end
+    else # Inference fails with the previous branch on Julia 0.5
+      if isdefined(f, :instance)
+          U = Core.Inference.return_type(f.instance, map(eltype_nullable, xs.parameters))
+          isleaftype(U) || (U = Union{})
+      elseif F === DataType # Function is a constructor
+          U = f.parameters[1]
+      else
+          U = Union{}
+      end
+      return quote
+          if (&)($(checknull...))
+              return Nullable(f($(args...)))
+          else
+              return Nullable{$U}()
           end
       end
     end
